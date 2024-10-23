96123806b58fac723dd568c44b46c85bdbe674b7
sagarpoudel638 <sagarpoudel638@gmail.com>
2024-10-23 21:43:26 +1100

    first commit

---
+++ src/pages/MyPostPage.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Header from "../components/Navbar";
+import PostCard from "../components/PostCard";
+import Footer from "../components/footer";
+import { FloatingButton } from "../components/FloatingButton";
+import { Link } from "react-router-dom";
+import { fetchMyPost } from "../utils/axiosHelper";
+
+const MyPostPage = () => {
+  const [posts,setPosts]= useState([]);
+  const fillPosts = async()=>{
+    const response = await fetchMyPost();
+    setPosts(response.data);
+  }
+  
+  useEffect(()=>{
+fillPosts();
+
+  },[])
+  return (
+    <>
+      <Header />
+      <Link to="/mypost/create">
+        <FloatingButton />
+      </Link>
+      <Container
+        fluid
+        className="d-flex justify-content-center align-items-center"
+        style={{ width: "66%", margin: "0 auto" }}
+      >
+        <Row className="mt-4">
+          <Col className="text-center">
+            <div className="d-flex gap-4 flex-wrap justify-content-center align-items-center">
+            {posts?.map(post => (
+        <PostCard key={post.id} post={post} />
+      ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </>
+  );
+};
+
+export default MyPostPage;
